7b018c1446985892821cbc04524a88240bdd0922
Naaman <naamanh10@gmail.com>
2025-05-16 14:06:33 +0100

    Form styles

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -60,13 +60,7 @@ export default function ContactForm() {
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
         <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
           <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-          <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-        <div 
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#051D40] to-[#1C3BD4] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" 
-          style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}
-            />
-          </div>
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900">Contact the team</h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
               We'd love to hear from you. Please fill out this form or shoot us an email.
             </p>
@@ -100,6 +94,12 @@ export default function ContactForm() {
                   </svg>
                 </dt>
                 <dd><a className="hover:text-gray-900" href="mailto:info@alperformance.co.uk">info@alperformance.co.uk</a></dd>
+                <div className="absolute inset-x-0 top-[calc(50%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(50%-30rem)]" aria-hidden="true">
+                <div 
+                  className="relative left-[calc(5%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#1C3BD4] to-[#051D40] opacity-30 sm:left-[calc(5%+36rem)] sm:w-[72.1875rem]" 
+                  style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}
+                />
+              </div>
               </div>
             </dl>
           </div>
